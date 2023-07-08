f4fec2b4e871dc08dae85b9ccc1d7c3c79d5c32a
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-08 14:16:19 +0700

    fix

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ function uploadFile() {
 
 function previewFile() {
     var preview = document.getElementById('photo');
-    var file    = document.querySelector('input[type=file]').files[0];
-    var reader  = new FileReader();
-  
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
     reader.onloadend = function () {
-      preview.src = reader.result;
+        preview.src = reader.result;
     }
-  
+
     if (file) {
-      reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
     } else {
-      preview.src = "";
+        preview.src = "";
     }
-  }
+}
