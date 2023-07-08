2750a55d235346c8e8548b8dd5963cd61669f1fe
ChuaTeBongToi <hbaoanh2k9@gmail.com>
2023-07-08 13:38:48 +0700

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+function uploadFile() {
+    document.getElementById("input-file").click();
+}
+
+function previewFile() {
+    var preview = document.getElementById('photo');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result;
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+  }
